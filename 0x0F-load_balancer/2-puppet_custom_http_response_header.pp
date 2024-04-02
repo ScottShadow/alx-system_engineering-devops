4b12951c87c967c04075c559447e4ee2e1c266be
ScottShadow <rudaseswascottmc@gmail.com>
2024-04-02 10:14:12 +0000

    puppet config file for servers

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,18 @@
 # Custom HTTP header in a nginx server
 
+# update ubuntu server
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
+}
+->
+# install nginx web server on server
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
+}
+->
 # custom Nginx response header (X-Served-By: hostname)
 file_line { 'add HTTP header':
   ensure => 'present',
