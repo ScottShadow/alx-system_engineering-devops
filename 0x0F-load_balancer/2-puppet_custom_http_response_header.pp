8d7bca337a5a8f2a98993ee281303fdb5e8df47c
ScottShadow <rudaseswascottmc@gmail.com>
2024-04-02 10:12:30 +0000

    puppet config file for servers

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,16 @@
+# Custom HTTP header in a nginx server
+
+# custom Nginx response header (X-Served-By: hostname)
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
+}
+->
+# start service
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
+}
